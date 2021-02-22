4f5444cbaba97afab83300fb7c37def980819ebc
a-seriz <dummymail@seriza.wa>
2021-02-22 09:34:43 +0900

    session_start関数が最初にないとエラーが出るらしい（localhostなら出ない）ので修正

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+	//csrf対策のtokenをセット
+	session_start();
+	if(!isset($_SESSION["csrf_token"])){
+		$_SESSION["csrf_token"] = bin2hex(random_bytes(32));
+	}
+	
+?>
 <!DOCTYPE html>
 <meta charset="utf-8">
 <link rel="stylesheet" href="css/index.css" type="text/css">
@@ -8,11 +16,6 @@
 	define('FILE_PATH',"./logs/messages.csv");
 	date_default_timezone_set('Asia/Tokyo');
 	
-	//csrf対策のtokenをセット
-	session_start();
-	if(!isset($_SESSION["csrf_token"])){
-		$_SESSION["csrf_token"] = bin2hex(random_bytes(32));
-	}
 	
 	
 	
