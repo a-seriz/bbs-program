0bb53548db0348823777be06122ebfc356a519db
a-seriz <dummymail@seriza.wa>
2021-02-27 11:26:35 +0900

    掲示板投稿時のrss対応実装を開始

---
+++ functions/bbs-rss.php
@@ -0,0 +1,9 @@
+<?php
+	class BbsUpdateRss{
+			
+			protected $bbs_title,$bbs_url,$bbs_description,$rss_file_name;
+			function __construct($bbs_title,$bbs_url,$bbs_description,$rss_file_name){	
+				
+			}
+	}
+?>
